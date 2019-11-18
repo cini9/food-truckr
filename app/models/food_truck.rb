5ababa6299af222829c5c9ae113b060b8634699e
Francois Capel <fanch.capel@gmail.com>
2019-11-18 17:20:38 +0100

    validation and dependencies of models completed

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -1,2 +1,8 @@
 class FoodTruck < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  has_many :users, through: :reservations
+  validates :name, presence: true, uniqueness: true
+  validates :category, inclusion: { in: [ "italian", "turkish", "belgian", "chinese", "japanese", "french", "burgers", "vegan" ] }
+  validates :price, presence: true
+  validates :city, presence: true
 end

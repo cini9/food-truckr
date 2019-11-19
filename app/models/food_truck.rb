cd74c8fa2228d8279a06d8113f389bac3ab84053
Sinan Ucak <ucak.sinan@gmail.com>
2019-11-19 10:15:20 +0100

    seed file updated

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -1,11 +1,13 @@
 class FoodTruck < ApplicationRecord
 
+  CATEGORY = [ "italian", "turkish", "belgian", "chinese", "japanese", "french", "burgers", "vegan" ]
+
   mount_uploader :photo, PhotoUploader
 
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
   validates :name, presence: true, uniqueness: true
-  validates :category, inclusion: { in: [ "italian", "turkish", "belgian", "chinese", "japanese", "french", "burgers", "vegan" ] }
+  validates :category, inclusion: { in: CATEGORY }
   validates :price, presence: true
   validates :city, presence: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Food truck seed
+
+FoodTruck.destroy_all
+
+10.times do |foodtruck|
+  foodtruck = FoodTruck.new({
+    name: Faker::Restaurant.name,
+    category: FoodTruck::CATEGORY.sample,
+    price: Faker::Number.within(range: 100..1000),
+    city: "Lausanne",
+    description: Faker::Lorem.paragraph
+  })
+  foodtruck.save!
+end

fc7f0d78188b67f8743bbc634d84b698b6adfd84
Francois Capel <fanch.capel@gmail.com>
2019-11-19 14:23:13 +0100

    added reservations new and create please check

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,19 @@
 class ReservationsController < ApplicationController
+
+  def new
+    @foodtruck = Foodtruck.find(params[:foodtruck_id])
+    @reservation = Reservation.new
+  end
+
+  def create
+    @foodtruck = Foodtruck.find(params[:foodtruck_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.foodtruck = @foodtruck
+    if @reservation.save
+      redirect_to foodtruck_path(@foodtruck)
+    else
+      render "foodtrucks/show"
+
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'food_trucks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :food_trucks, only: [:index, :show, :new, :create]
+  resources :reservations, only: [:new, :create]
+
 end

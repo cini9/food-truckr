e21841608b5ae1c3c7bfcfb083eb9d7ab8e4fe59
Cecile Colombo <cecile.colombo@hotmail.fr>
2019-11-21 12:33:52 +0100

    create 2 new users, 1 foodtruck and 1 reservation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
 
-# require 'faker'
+puts "Cleaning database..."
+Reservation.destroy_all
+FoodTruck.destroy_all
+User.destroy_all
 
-# Food truck seed
+puts "Creating users"
+user1 = User.create(email: 'henri@jaimelafete.ch', password: '123456' , food_truck_owner: false, name: 'Henri', description: "Hello, je suis Henri, de Gland. J'aime inviter tous mes amis à des soirées spéciales foodtruck pour leur faire goûter des spécalités du monde entier, mais en Suisse seulement! A tout bientôt!")
+user2 = User.create(email: 'hello@jaimelafete.ch', password: '123456' , food_truck_owner: false, name: 'Henri', description: "Hello, je suis Henri, de Gland. J'aime inviter tous mes amis à des soirées spéciales foodtruck pour leur faire goûter des spécalités du monde entier, mais en Suisse seulement! A tout bientôt!")
 
-# FoodTruck.destroy_all
+puts "Creating foodtrucks"
+food_truck1 = FoodTruck.create(name: 'JojoLaFrite', category: 'belgian', city: 'Bruxelles', description: 'Je suis votre partenaire idéal pour une soirée mémorable! Goutez à nos frites formidables!', user_id: user2.id, photo: "" , price_cents: 8500)
 
-# 10.times do |foodtruck|
-#   foodtruck = FoodTruck.new({
-#     name: Faker::Restaurant.name,
-#     category: FoodTruck::CATEGORY.sample,
-#     price: Faker::Number.within(range: 100..1000),
-#     city: "Lausanne",
-#     description: Faker::Lorem.paragraph
-#   })
-#   foodtruck.save!
-# end
+puts "Creating reservations"
+reservation1 = Reservation.create(checkin_date: '27/11/2019', checkout_date: '28/11/2019', accepted_at: nil, paid_at: nil, state: "Not accepted yet", amount_cents: 8500, user_id: user1.id, food_truck_id:food_truck1.id)
